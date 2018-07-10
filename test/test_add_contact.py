b3c45a19d9fa63c07d8b6f0fdc6e1a4ee84fe4f8
FedorovskayaElena <afel@mail.ru>
2018-07-09 21:53:55 -0700

    Задание 19: Теперь для добавления и групп, и контактов тестовые данные подтягиваются из json и py с помощью pytest_generate_tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
 
 
-@pytest.mark.parametrize("new_contact", test_data1, ids=[repr(x) for x in test_data1])
-def test_add_contact(app, new_contact):
+# запуск тестовой функции на данных в файле питон
+def test_add_contact_module(app, data_contacts):
+    new_contact = data_contacts
     old_contacts_list = app.contact.get_contacts_list()
     app.contact.create(new_contact)
     assert len(old_contacts_list) + 1 == app.contact.count()
@@ -16,3 +16,17 @@ def test_add_contact(app, new_contact):
     # print("Sorted:\n%s\n%s" % (sorted(old_contacts_list, key=Contact.id_or_max), sorted(new_contacts_list, key=Contact.id_or_max)))
     assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
 
+
+# запуск тестовой функции на данных в файле JSON
+def test_add_contact_json(app, json_contacts):
+    new_contact = json_contacts
+    old_contacts_list = app.contact.get_contacts_list()
+    app.contact.create(new_contact)
+    assert len(old_contacts_list) + 1 == app.contact.count()
+    new_contacts_list = app.contact.get_contacts_list()
+    # print("\n%s %s", (new_contact.bdayoption, new_contact.bmonthoption))
+    # print("\n%s %s", (new_contact.bdayoption, new_contact.bmonthoption))
+    old_contacts_list.append(new_contact)
+    # print("Appended:\n%s" % old_contacts_list)
+    # print("Sorted:\n%s\n%s" % (sorted(old_contacts_list, key=Contact.id_or_max), sorted(new_contacts_list, key=Contact.id_or_max)))
+    assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
